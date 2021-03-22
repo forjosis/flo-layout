6930cf2468feb4c147c106d93cc76ad0eeb0b6bb
forjosis <forjosis.work@gmail.com>
2021-03-23 03:08:42 +0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+//
+// Variables ===================================
+//
+let 
+    // plugins
+    { src, dest, watch, series, parallel } = require('gulp'),
+    sass = require('gulp-sass'),
+    // parts
+    dist_folder = 'dist',
+    source_folder  = 'src',
+    path = {
+        src:{
+            scss: source_folder + '/*.scss'
+        },
+        watch: {
+            scss: source_folder + '/*.scss'
+        },
+        dist: {
+            css: dist_folder + '/'
+        }
+    };
+
+
+//
+// Tasks ===================================
+//
+function styles(){
+    return src(path.src.scss)
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(dest(path.dist.css))
+}
+
+function watchFiles(){
+	watch([path.watch.scss], styles);
+}
+
+exports.styles = styles;
+exports.default = series(styles, parallel(watchFiles));
